2fc01fea9f3933fff45439bfc8fcbea62f3388ab
AndrewFromMelbourne <andrew@rpi512.(none)>
2013-07-26 15:05:20 +1000

    Allow choice of image type for snapshot.

--- raspi2png.c
+++ raspi2png.c
@@ -7,35 +7,178 @@
 
 #include "bcm_host.h"
 
+//-----------------------------------------------------------------------
+
+#define ALIGN_TO 16
+
 #ifndef ALIGN_UP
 #define ALIGN_UP(x,y)  ((x + (y)-1) & ~((y)-1))
 #endif
 
 //-----------------------------------------------------------------------
 
+const char* program = NULL;
+
+//-----------------------------------------------------------------------
+
+static const char* imageTypeNames[] =
+{
+    "RGB565",
+    "RGB888",
+    "RGBA32"
+};
+
+static const VC_IMAGE_TYPE_T imageTypes[] =
+{
+    VC_IMAGE_RGB565,
+    VC_IMAGE_RGB888,
+    VC_IMAGE_RGBA32
+};
+
+static int imageBytesPerPixel[] =
+{
+    2,
+    3,
+    4
+};
+
+static size_t imageEntries = sizeof(imageTypeNames)/sizeof(imageTypeNames[0]);
+//-----------------------------------------------------------------------
+
+void
+pngWriteImageRGB565(
+    int width,
+    int height,
+    int pitch,
+    void* buffer,
+    png_structp pngPtr,
+    png_infop infoPtr)
+{
+    int rowLength = 3 * width;
+    uint8_t *imageRow = malloc(rowLength);
+
+    if (imageRow == NULL)
+    {
+        fprintf(stderr, "%s: unable to allocated row buffer\n", program);
+        exit(EXIT_FAILURE);
+    }
+
+    int y = 0;
+    for (y = 0; y < height; y++)
+    {
+        int x = 0;
+        for (x = 0; x < width; x++)
+        {
+            uint16_t pixels = *(uint16_t*)(buffer + (y * pitch) + (x * 2));
+            int index = x * 3;
+
+            imageRow[index] =  ((pixels >> 11) * 0xFF) / 0x1F;
+            imageRow[index + 1] =  (((pixels >> 5) & 0x3F) * 0xFF) / 0x3F;
+            imageRow[index + 2] =  ((pixels & 0x1F) * 0xFF) / 0x1F;
+        }
+        png_write_row(pngPtr, imageRow);
+
+    }
+
+    free(imageRow);
+}
+
+//-----------------------------------------------------------------------
+
+void
+pngWriteImageRGB888(
+    int width,
+    int height,
+    int pitch,
+    void* buffer,
+    png_structp pngPtr,
+    png_infop infoPtr)
+{
+    int y = 0;
+    for (y = 0; y < height; y++)
+    {
+        png_write_row(pngPtr, buffer + (pitch * y));
+    }
+}
+
+//-----------------------------------------------------------------------
+
+void
+pngWriteImageRGBA32(
+    int width,
+    int height,
+    int pitch,
+    void* buffer,
+    png_structp pngPtr,
+    png_infop infoPtr)
+{
+    int rowLength = 3 * width;
+    uint8_t *imageRow = malloc(rowLength);
+
+    if (imageRow == NULL)
+    {
+        fprintf(stderr, "%s: unable to allocated row buffer\n", program);
+        exit(EXIT_FAILURE);
+    }
+
+    int y = 0;
+    for (y = 0; y < height; y++)
+    {
+        int x = 0;
+        for (x = 0; x < width; x++)
+        {
+            uint8_t *pixels = (uint8_t*)(buffer + (y * pitch) + (x * 4));
+            int index = x * 3;
+
+            imageRow[index] =  pixels[0];
+            imageRow[index + 1] =  pixels[1];
+            imageRow[index + 2] =  pixels[2];
+        }
+        png_write_row(pngPtr, imageRow);
+
+    }
+
+    free(imageRow);
+}
+
+//-----------------------------------------------------------------------
+
 int main(int argc, char *argv[])
 {
-    int opt;
-    char *pngname = "snapshot.png";
-    int align_to = 16;
-    int requested_width = 0;
-    int requested_height = 0;
+    int opt = 0;
+
+    char *pngName = "snapshot.png";
+    int requestedWidth = 0;
+    int requestedHeight = 0;
     bool verbose = false;
+
+    const char* imageTypeName = "RGB888";
+    VC_IMAGE_TYPE_T imageType = VC_IMAGE_MIN;
+    int bytesPerPixel  = 0;
+
+    int result = 0;
+
+    program = argv[0];
 
     //-------------------------------------------------------------------
 
-    while ((opt = getopt(argc, argv, "h:p:vw:")) != -1)
+    while ((opt = getopt(argc, argv, "h:p:t:vw:")) != -1)
     {
         switch (opt)
         {
         case 'h':
 
-            requested_height = atoi(optarg);
+            requestedHeight = atoi(optarg);
             break;
 
         case 'p':
 
-            pngname = optarg;
+            pngName = optarg;
+            break;
+
+        case 't':
+
+            imageTypeName = optarg;
             break;
 
         case 'v':
@@ -45,13 +188,13 @@ int main(int argc, char *argv[])
 
         case 'w':
 
-            requested_width = atoi(optarg);
+            requestedWidth = atoi(optarg);
             break;
 
         default:
 
-            fprintf(stderr, "Usage: %s [-p pngname] [-v]", argv[0]);
-            fprintf(stderr, " [-w <width>] [-h <height>]\n");
+            fprintf(stderr, "Usage: %s [-p pngname] [-v]", program);
+            fprintf(stderr, " [-w <width>] [-h <height>] [-t <type>]\n");
 
             fprintf(stderr, "    -p - name of png file to create\n");
             fprintf(stderr, "    -v - verbose\n");
@@ -61,9 +204,42 @@ int main(int argc, char *argv[])
             fprintf(stderr,
                     "    -w - image width (default is screen width)\n");
 
+            fprintf(stderr, "    -t - type of image captured\n");
+            fprintf(stderr, "         can be one of the following:");
+
+            size_t entry = 0;
+            for (entry = 0; entry < imageEntries; entry++)
+            {
+                fprintf(stderr, " %s", imageTypeNames[entry]);
+            }
+            fprintf(stderr, "\n");
+
             exit(EXIT_FAILURE);
             break;
         }
+    }
+
+    //-------------------------------------------------------------------
+
+    size_t entry = 0;
+    for (entry = 0; entry < imageEntries; entry++)
+    {
+        if (strcmp(imageTypeName, imageTypeNames[entry]) == 0)
+        {
+            imageType = imageTypes[entry];
+            bytesPerPixel =  imageBytesPerPixel[entry];
+            break;
+        }
+    }
+
+    if (imageType == VC_IMAGE_MIN)
+    {
+        fprintf(stderr,
+                "%s: unknown image type %s\n",
+                program,
+                imageTypeName);
+
+        exit(EXIT_FAILURE);
     }
 
     //-------------------------------------------------------------------
@@ -73,67 +249,75 @@ int main(int argc, char *argv[])
     DISPMANX_DISPLAY_HANDLE_T displayHandle = vc_dispmanx_display_open(0);
 
     DISPMANX_MODEINFO_T modeInfo;
-    int getInfoReturned = vc_dispmanx_display_get_info(displayHandle,
-                                                       &modeInfo);
+    result = vc_dispmanx_display_get_info(displayHandle, &modeInfo);
 
-    if (getInfoReturned != 0)
+    if (result != 0)
     {
-        fprintf(stderr, "%s: unable to get display information\n", argv[0]);
+        fprintf(stderr, "%s: unable to get display information\n", program);
         exit(EXIT_FAILURE);
     }
 
     int width = modeInfo.width;
     int height = modeInfo.height;
 
-    if (requested_width > 0)
+    if (requestedWidth > 0)
     {
-        width = requested_width;
+        width = requestedWidth;
     }
 
-    if (requested_height > 0)
+    if (requestedHeight > 0)
     {
-        height = requested_height;
+        height = requestedHeight;
     }
-    else if (requested_width > 0)
+    else if (requestedWidth > 0)
     {
-        height = (modeInfo.height * requested_width) / modeInfo.width;
+        height = (modeInfo.height * requestedWidth) / modeInfo.width;
     }
 
-    int pitch = 3 * ALIGN_UP(width, align_to);
+    int pitch = bytesPerPixel * ALIGN_UP(width, ALIGN_TO);
 
     if (verbose)
     {
         printf("screen width = %d\n", modeInfo.width);
         printf("screen height = %d\n", modeInfo.height);
-        printf("requested width = %d\n", requested_width);
-        printf("requested height = %d\n", requested_height);
+        printf("requested width = %d\n", requestedWidth);
+        printf("requested height = %d\n", requestedHeight);
         printf("image width = %d\n", width);
         printf("image height = %d\n", height);
+        printf("image type = %s\n", imageTypeName);
+        printf("bytes per pixel = %d\n", bytesPerPixel);
         printf("pitch = %d\n", pitch);
     }
 
-    uint32_t vcImagePtr;
-    void *imagePtr = malloc(pitch * height);
+    void *dmxImagePtr = malloc(pitch * height);
 
-    if (imagePtr == NULL)
+    if (dmxImagePtr == NULL)
     {
-        fprintf(stderr, "%s: unable to allocated image buffer\n", argv[0]);
+        fprintf(stderr, "%s: unable to allocated image buffer\n", program);
         exit(EXIT_FAILURE);
     }
 
+    uint32_t vcImagePtr = 0;
     DISPMANX_RESOURCE_HANDLE_T resourceHandle;
-    resourceHandle = vc_dispmanx_resource_create(VC_IMAGE_RGB888,
+    resourceHandle = vc_dispmanx_resource_create(imageType,
                                                  width,
                                                  height,
                                                  &vcImagePtr);
 
-    int result = 0;
-    
     result = vc_dispmanx_snapshot(displayHandle, resourceHandle, 0);
 
     if (verbose)
     {
         printf("vc_dispmanx_snapshot() returned %d\n", result);
+    }
+
+    if (result != 0)
+    {
+        vc_dispmanx_resource_delete(resourceHandle);
+        vc_dispmanx_display_close(displayHandle);
+
+        fprintf(stderr, "%s: vc_dispmanx_snapshot() failed\n", program);
+        exit(EXIT_FAILURE);
     }
 
     VC_RECT_T rect;
@@ -144,18 +328,51 @@ int main(int argc, char *argv[])
         printf("vc_dispmanx_rect_set() returned %d\n", result);
     }
 
+    if (result != 0)
+    {
+        vc_dispmanx_resource_delete(resourceHandle);
+        vc_dispmanx_display_close(displayHandle);
+
+        fprintf(stderr, "%s: vc_dispmanx_rect_set() failed\n", program);
+        exit(EXIT_FAILURE);
+    }
+
     result = vc_dispmanx_resource_read_data(resourceHandle,
                                             &rect,
-                                            imagePtr,
+                                            dmxImagePtr,
                                             pitch);
+
+
+    if (result != 0)
+    {
+        vc_dispmanx_resource_delete(resourceHandle);
+        vc_dispmanx_display_close(displayHandle);
+
+        fprintf(stderr,
+                "%s: vc_dispmanx_resource_read_data() failed\n",
+                program);
+
+        exit(EXIT_FAILURE);
+    }
 
     if (verbose)
     {
         printf("vc_dispmanx_resource_read_data() returned %d\n", result);
     }
 
-    vc_dispmanx_resource_delete(resourceHandle);
-    vc_dispmanx_display_close(displayHandle);
+    result = vc_dispmanx_resource_delete(resourceHandle);
+
+    if (verbose)
+    {
+        printf("vc_dispmanx_resource_delete() returned %d\n", result);
+    }
+
+    result = vc_dispmanx_display_close(displayHandle);
+
+    if (verbose)
+    {
+        printf("vc_dispmanx_display_close() returned %d\n", result);
+    }
 
     //-------------------------------------------------------------------
 
@@ -168,7 +385,7 @@ int main(int argc, char *argv[])
     {
         fprintf(stderr,
                 "%s: unable to allocated PNG write structure\n",
-                argv[0]);
+                program);
 
         exit(EXIT_FAILURE);
     }
@@ -179,25 +396,25 @@ int main(int argc, char *argv[])
     {
         fprintf(stderr,
                 "%s: unable to allocated PNG info structure\n",
-                argv[0]);
+                program);
 
         exit(EXIT_FAILURE);
     }
 
     if (setjmp(png_jmpbuf(pngPtr)))
     {
-        fprintf(stderr, "%s: unable to create PNG\n", argv[0]);
+        fprintf(stderr, "%s: unable to create PNG\n", program);
         exit(EXIT_FAILURE);
     }
 
-    FILE *pngfp = fopen(pngname, "wb");
+    FILE *pngfp = fopen(pngName, "wb");
 
     if (pngfp == NULL)
     {
         fprintf(stderr,
                 "%s: unable to create %s - %s\n",
-                argv[0],
-                pngname,
+                program,
+                pngName,
                 strerror(errno));
 
         exit(EXIT_FAILURE);
@@ -218,10 +435,41 @@ int main(int argc, char *argv[])
 
     png_write_info(pngPtr, infoPtr);
 
-    int y;
-    for (y = 0; y < height; y++)
+    switch(imageType)
     {
-        png_write_row(pngPtr, imagePtr + (pitch * y));
+    case VC_IMAGE_RGB565:
+
+        pngWriteImageRGB565(width,
+                            height,
+                            pitch,
+                            dmxImagePtr,
+                            pngPtr,
+                            infoPtr);
+        break;
+
+    case VC_IMAGE_RGB888:
+
+        pngWriteImageRGB888(width,
+                            height,
+                            pitch,
+                            dmxImagePtr,
+                            pngPtr,
+                            infoPtr);
+        break;
+
+    case VC_IMAGE_RGBA32:
+
+        pngWriteImageRGBA32(width,
+                            height,
+                            pitch,
+                            dmxImagePtr,
+                            pngPtr,
+                            infoPtr);
+        break;
+
+    default:
+
+        break;
     }
 
     png_write_end(pngPtr, NULL);
@@ -230,7 +478,7 @@ int main(int argc, char *argv[])
 
     //-------------------------------------------------------------------
 
-    free(imagePtr);
+    free(dmxImagePtr);
 
     return 0;
 }
