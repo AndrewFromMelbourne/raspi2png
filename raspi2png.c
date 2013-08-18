70d0a97620a170928120ad755ed903698d354c71
AndrewFromMelbourne <arduncan@bigpond.com>
2013-08-18 20:57:25 +1000

    Added MIT License

--- raspi2png.c
+++ raspi2png.c
@@ -1,3 +1,30 @@
+//-------------------------------------------------------------------------
+//
+// The MIT License (MIT)
+//
+// Copyright (c) 2013 Andrew Duncan
+//
+// Permission is hereby granted, free of charge, to any person obtaining a
+// copy of this software and associated documentation files (the
+// "Software"), to deal in the Software without restriction, including
+// without limitation the rights to use, copy, modify, merge, publish,
+// distribute, sublicense, and/or sell copies of the Software, and to
+// permit persons to whom the Software is furnished to do so, subject to
+// the following conditions:
+//
+// The above copyright notice and this permission notice shall be included
+// in all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+// OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+// MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+// IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+// CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+// TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+// SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+//
+//-------------------------------------------------------------------------
+
 #include <math.h>
 #include <png.h>
 #include <stdbool.h>
@@ -308,7 +335,9 @@ int main(int argc, char *argv[])
                                                  height,
                                                  &vcImagePtr);
 
-    result = vc_dispmanx_snapshot(displayHandle, resourceHandle, 0);
+    result = vc_dispmanx_snapshot(displayHandle,
+                                  resourceHandle,
+                                  DISPMANX_NO_ROTATE);
 
     if (verbose)
     {
@@ -330,6 +359,11 @@ int main(int argc, char *argv[])
     if (verbose)
     {
         printf("vc_dispmanx_rect_set() returned %d\n", result);
+		printf("rect = { %d, %d, %d, %d }\n",
+			   rect.x,
+			   rect.y,
+			   rect.width,
+			   rect.height);
     }
 
     if (result != 0)
