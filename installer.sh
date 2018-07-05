3ae9469c01fefbb44ed62cc906c501a6ac9751dc
MileSacul <11881208+lashower@users.noreply.github.com>
2018-07-05 12:23:55 -0400

    Update installer.sh
    Switched sudo make to sudo make install

--- installer.sh
+++ installer.sh
@@ -2,9 +2,7 @@ sudo apt-get -y install libpng12-dev
 cd ~
 git clone https://github.com/AndrewFromMelbourne/raspi2png.git
 cd raspi2png
-sudo make
-sudo chmod 755 raspi2png
-sudo mv raspi2png /usr/bin
+sudo make install
 cd ..
 rm -fr raspi2png
 raspi2png -H
