8ae3a4987d88e96a2eaf524600bfdc91bf9742a2
mstroh76 <m.stroh76@gmail.com>
2020-08-20 12:47:03 +0200

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -1,6 +1,6 @@
 sudo apt-get -y install libpng12-dev git-core
 cd ~
-git clone https://github.com/AndrewFromMelbourne/raspi2png.git
+git clone https://github.com/GrazerComputerClub/raspi2png.git
 cd raspi2png
 make
 sudo make install
